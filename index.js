a5187817ac57f5e602a4f96a3dd5bed299442efa
Jason <jason.981214@gmail.com>
2020-05-19 16:52:21 -0400

    added url verification

--- index.js
+++ index.js
@@ -9,8 +9,15 @@ var urlList = {
     urls: JSON.parse(localStorage.getItem("urls")),
 
     addUrl: function(urlText) {
-        this.urls.push(urlText);
-        localStorage.setItem("urls", JSON.stringify(this.urls));
+        if (verification.isUrl(urlText))
+        {
+            this.urls.push(urlText);
+            localStorage.setItem("urls", JSON.stringify(this.urls));
+        }
+        else {
+            console.log(verification.isUrl(urlText));    
+            window.alert("invalid url");  
+        }
     },
 
     deleteUrl: function(position) {
@@ -97,6 +104,7 @@ var view = {
 
     setUpEventListener: function() {
         var urlUl = document.querySelector("ul");
+        var enterKey = document.getElementById("urlTextInput");
 
         urlUl.addEventListener("click", function(event) {
             var elementClicked = event.target;
@@ -104,8 +112,31 @@ var view = {
                 handlers.deleteUrl(parseInt(elementClicked.parentNode.parentNode.id));
             }
         });
+
+        enterKey.addEventListener("keyup", function(event){
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                document.getElementById("blockButton").click();
+            }
+        });
     }
 };
+
+var verification = {
+    isUrl: function(str) 
+    {
+        regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+        if (regexp.test(str))
+        {
+          return true;
+        }
+        else
+        {
+          return false;
+        }
+    }
+}
+
 view.setUpEventListener();
 
 if (localStorage.getItem("urls") === "null") {
