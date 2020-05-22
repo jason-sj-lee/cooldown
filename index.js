84a541e05dcf5ff23aafc066fcee6b1da143c919
Jason <jason.981214@gmail.com>
2020-05-22 14:51:19 -0400

    separated functionality for list page and main page

--- index.js
+++ index.js
@@ -1,76 +1,3 @@
-/////////////////////////////////////
-// functionality for index page //
-
-var hour, minute, second, btn;
-
-window.onload = toggle;
-
-// each of the time input field
-hour = document.getElementById("hour");
-minute = document.getElementById("minute");
-second = document.getElementById("second");
-
-// blur: when out of focus
-hour.addEventListener("blur", storeTime);
-minute.addEventListener("blur", storeTime);
-second.addEventListener("blur", storeTime);
-
-displayTime();
-
-// button toggle
-function toggle() {
-    var btn = document.getElementById("mybtn");
-    if (btn.value === "on") {
-        btn.innerHTML = "stop";
-        btn.value = "off";
-        btn.addEventListener("click", toggle);
-        addReadOnly();
-    }
-    else {
-        btn.innerHTML = "start";
-        btn.value = "on";
-        btn.addEventListener("click", toggle);
-        removeReadOnly();
-    }
-}
-
-// display selected time from localStorage
-function displayTime() {
-    hour.value = localStorage.getItem('hr');
-    minute.value = localStorage.getItem('min');
-    second.value = localStorage.getItem('sec');
-}
-
-// store selected time in localStorage
-function storeTime() {
-    // dont know why this is there 
-    localStorage.removeItem('hour');
-    localStorage.removeItem('minute');
-    localStorage.removeItem('second');
-
-    // storing time
-    localStorage.setItem('hr', hour.value);
-    localStorage.setItem('min', minute.value);
-    localStorage.setItem('sec', second.value);
-}
-
-// read-only functionality for time input
-function addReadOnly() {
-    // apply read-only for time input
-    hour.readOnly = true;
-    minute.readOnly = true;
-    second.readOnly = true;
-}
-
-function removeReadOnly() {
-    // remove read-only for time input
-    hour.readOnly = false;
-    minute.readOnly = false;
-    second.readOnly = false;
-}
-
-
-
 ///////////////////////////////////
 // functionality for lists page //
 var blockButton = document.getElementById("blockButton").addEventListener("click", function() {handlers.addUrl()});
@@ -92,7 +19,7 @@ var urlList = {
             this.urls.push(urlText);
             localStorage.setItem("urls", JSON.stringify(this.urls));
             localStorage.setItem("urlsFormatted", JSON.stringify(format.formatToUrl(this.urls)));
-            chrome.runtime.reload()
+            //chrome.runtime.reload()
         //}
         // else {
         //     console.log(verification.isUrl(urlText));    
@@ -104,7 +31,7 @@ var urlList = {
         this.urls.splice(position, 1);
         localStorage.setItem("urls", JSON.stringify(this.urls));
         localStorage.setItem("urlsFormatted", JSON.stringify(format.formatToUrl(this.urls)));
-        chrome.runtime.reload()
+        //chrome.runtime.reload()
     }
 };
 
@@ -242,6 +169,5 @@ if (localStorage.getItem("urls") === "null") {
 if (localStorage.getItem("undefined") === "null") {
     localStorage.setItem("urlsFormatted", JSON.stringify([]));
 }
-
 
 view.displayLocalStorage();
